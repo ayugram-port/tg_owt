52ad0aefbcc9b39413ff739db2eed7b244fceb4f
kaeeraa <kaeeraa@nebula-nook.ru>
2024-09-18 22:31:17 +0300

    flake: init and fill up

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Desktop Telegram client with good customization and Ghost mode.";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          tg_owt = (pkgs.callPackage ./default.nix { });
+          default = tg_owt;
+        };
+      }
+    );
+}
